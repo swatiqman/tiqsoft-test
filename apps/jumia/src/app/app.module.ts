d5bd10c03225cd63e25065a4251760cf708efa07
Stephen Affanyi <affanyisy91@gmail.com>
2021-03-22 15:09:50 +0000

    jumia config for root

--- apps/jumia/src/app/app.module.ts
+++ apps/jumia/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { JumiaModule } from '@tiq-soft-test/shells/jumia'
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, JumiaModule],
+  imports: [BrowserModule, JumiaModule.forRoot(environment)],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- libs/shells/jumia/src/lib/jumia.module.ts
+++ libs/shells/jumia/src/lib/jumia.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JumiaComponent } from './jumia.component';
 import { JumiaRoutingModule } from './jumia-routing.module'
 import { TUI_ICONS_PATH, iconsPathFactory, TuiRootModule } from '@taiga-ui/core';
+import { APP_CONFIG } from '@tiq-soft-test/app-config'
 
 
 @NgModule({
   imports: [CommonModule, JumiaRoutingModule, TuiRootModule],
   exports: [JumiaRoutingModule],
   declarations: [JumiaComponent],
-  providers: [{
-    provide: TUI_ICONS_PATH,
-    useValue: iconsPathFactory('assets/taiga-ui/icons/'),
-  }]
 })
-export class JumiaModule { }
+export class JumiaModule {
+
+  static forRoot(environment: unknown): ModuleWithProviders<JumiaModule> {
+    return {
+      ngModule: JumiaModule,
+      providers: [
+        { provide: APP_CONFIG, useValue: environment },
+        { provide: TUI_ICONS_PATH, useValue: iconsPathFactory('assets/taiga-ui/icons/') }
+      ]
+    }
+  }
+
+ }
