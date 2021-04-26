a6562c3db2b06be9d80b5091ce09f69ef2af912c
Stephen Affanyi <affanyisy91@gmail.com>
2021-04-26 16:28:37 +0000

    further config

--- libs/shells/mychurch/src/lib/mychurch.module.ts
+++ libs/shells/mychurch/src/lib/mychurch.module.ts
@@ -1,22 +1,37 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { MychurchComponent } from './mychurch.component';
 import { MychurchRoutingModule } from './mychurch-routing.module';
-// import { TuiRootModule } from '@taiga-ui/core/components';
-// import { TUI_ICONS_PATH } from '@taiga-ui/core/tokens';
-// import { iconsPathFactory } from '@taiga-ui/core/utils/miscellaneous';
 import { TUI_ICONS_PATH, iconsPathFactory, TuiRootModule } from '@taiga-ui/core';
+import { environment } from '../environments/mychurch-environment'
+import { ConfigModel, APP_CONFIG } from '@tiq-soft-test/models/core'
 
-
+const appConfig = (httpClient: HttpClient, config: ConfigModel) => {
+  return () => {
+    httpClient.get('app-config/config.json').toPromise().then(resp => {
+      Object.assign(config, resp)
+    }).catch(() => console.log('config file not available, please provide it'))
+  }
+}
 @NgModule({
-  imports: [CommonModule, MychurchRoutingModule, TuiRootModule,],
+  imports: [CommonModule, MychurchRoutingModule, TuiRootModule, HttpClientModule],
   exports: [MychurchRoutingModule],
   declarations: [MychurchComponent],
-  providers: [
-    {
-        provide: TUI_ICONS_PATH,
-        useValue: iconsPathFactory('assets/taiga-ui/icons/'),
-    },
-]
 })
-export class MychurchModule {}
+export class MychurchModule {
+  static forRoot(): ModuleWithProviders<MychurchModule> {
+    return {
+      ngModule: MychurchModule,
+      providers: [
+        { provide: APP_CONFIG, useValue: {...environment} },
+        { provide: APP_INITIALIZER,
+          useFactory: appConfig,
+          deps: [HttpClient, APP_CONFIG],
+          multi: true
+        },
+        { provide: TUI_ICONS_PATH, useValue: iconsPathFactory('assets/taiga-ui/icons/') },
+      ]
+    }
+  }
+}
